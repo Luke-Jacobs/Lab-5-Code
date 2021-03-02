740fc33a8059480d851e7242fdcdb2336fa404af
Arya Kothari <arya.kothari@gmail.com>
2021-03-02 15:20:13 -0500

    New files

---
+++ Bus.sv
@@ -0,0 +1,21 @@
+module Bus(input logic [15:0]  In_PC, 
+	input logic [15:0] In_ALU,
+	input logic [15:0]  In_MARMUX,
+	input logic [15:0]  In_MDR,
+	input logic [3:0] select_1hot,
+	output logic [15:0]  bus_out);
+	
+always_comb
+begin
+	bus_out = 4'bxxxx;//default case
+	unique case(select_1hot)
+		begin
+			4'b0001 : bus_out = In_PC; //encoding for each tristate buffer
+			4'b0010 : bus_out = In_ALU;
+			4'b0100 : bus_out = In_MARMUX;
+			4'b1000 : bus_out = In_MDR;
+			default : %display("bus error"); //for testing
+		end
+end
+
+endmodule
---
+++ Reg_File.sv
@@ -0,0 +1,7 @@
+module reg_file(input LD_REG,
+input logic[2:0] DR,
+input[2:0] In_SR1,
+input[2:0] In_SR2,
+ouput
+
+)
